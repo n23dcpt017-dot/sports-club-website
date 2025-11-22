2f986fb61e7f0fe955e9ca4640d36c5f3d1aa578
Vuong Thi Huong Giang <n23dcpt017@student.ptithcm.edu.vn>
2025-11-22 13:59:25 +0700

    Upload project

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+// Thêm base: '/tên-repo/' 
 export default defineConfig({
+  base: '/sports-club-website/',
   plugins: [react()],
 })
